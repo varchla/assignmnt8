2427d35e82c423211abb310febf77d9b4a6c0dd6
varchla <vthakur1207@gmail.com>
2018-06-26 22:06:22 +0530

    assignmnt done

---
+++ assignmnt8.py
@@ -0,0 +1,54 @@
+#ques 1
+
+print("time tuples is a way to represent time that is easy for understandpythan stores it in tuples")
+
+#ques 2
+
+import datetime,time
+print(time.asctime(time.localtime()))
+
+#ques 3
+
+import datetime
+from datetime import date
+d=date.today()
+print(d.month)
+
+#ques 4
+
+import datetime
+from datetime import date
+d=date.today()
+print(d.day)
+
+#ques 5
+
+import datetime
+from datetime import date
+d=date.today()
+print(d.day,"/",d.month,"/",d.year)
+
+#ques 6
+
+import time
+print(time.localtime())
+
+#ques 7
+
+n=int(input("enter n"))
+import math
+print(math.factorial(n))
+
+#ques 8
+
+a=int(input("enter no"))
+b=int(input("enter no"))
+import math
+print(math.gcd(a,b))
+
+#ques 9
+
+import os
+print(os.path)
+print(os.environ)
+
